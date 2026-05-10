1e30cf41e033a3b0220fa8eed1c90453479c1821
njevric <njevric9@gmail.com>
2026-05-10 22:01:13 +0200

    feat: not found page

--- src/client/router.ts
+++ src/client/router.ts
@@ -29,6 +29,11 @@ const router = createRouter({
       name: 'activity',
       component: () => import('./views/ActivityView.vue'),
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('./views/NotFoundView.vue'),
+    },
   ],
 })
 
